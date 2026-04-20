4de53938078fb57767f1b85251201408c0168859
Yuvam <yuvam05.ed@gmail.com>
2026-04-19 21:41:26 -0700

    Initial Commit

---
+++ lifeloop/lifeloopApp.swift
@@ -0,0 +1,17 @@
+//
+//  lifeloopApp.swift
+//  lifeloop
+//
+//  Created by Yuvam Bhargav on 4/19/26.
+//
+
+import SwiftUI
+
+@main
+struct lifeloopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
